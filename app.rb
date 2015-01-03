8c37aee1cd1b78a68d27c8a3f4bb7e4670d944dd
masanori_msl <masanori840816@gmail.com>
2015-01-04 00:42:24 +0900

    Moved functions of separating DB datas to app.rb
    DBから取得した値をIDや記事タイトルなど個別データに振り分ける機能を、blog.slimからapp.rbに移動

--- app.rb
+++ app.rb
@@ -9,6 +9,9 @@ require './models/dbAccessers'
 
 # TODO:テスト用に5件1ページで表示中。分量的にはこのままでもいいかも？
 POST_LIMIT_COUNT = 5
+POST_URL_DIR = "/article/"
+TAG_URL_DIR = "/tag/"
+POST_SHORT_LIMIT_SIZE = 300
 
 class MainApp < Sinatra::Base
 
@@ -18,27 +21,24 @@ class MainApp < Sinatra::Base
     intPageOffsetNum = getPageOffset(params[:page])
 
     getPagerCount(Post.count)
-    @aryPosts = Post.all.order(post_id: 'desc').limit(POST_LIMIT_COUNT).offset(intPageOffsetNum)
-    getLinkedTags
+    aryPosts = Post.all.order(post_id: 'desc').limit(POST_LIMIT_COUNT).offset(intPageOffsetNum)
+    getShortPosts(aryPosts)
+    getRightColumnData
     slim :blog
   end
   get '/article/:name' do
     @aryArticle = Post.where(post_id: params[:name])
     @aryTags = Tag.joins(:taglinks).select(:tag_name, :post_id, :tag_id).where(taglinks: {post_id: params[:name]})
-
-    # TODO: あとで統合
-    @aryAllTags = Tag.all.order(tag_name: 'asc')
-    # 最近の投稿から10件取得する.
-    @aryCurrentPosts = Post.select(:post_id, :post_title).order(updated_at: 'desc').limit(10)
-
+    getRightColumnData
     slim :article
   end
   get '/tag/:name' do
     intPageOffsetNum = getPageOffset(params[:page])
 
-    @aryPosts = Post.joins(:taglinks).where(taglinks: {tag_id: params[:name]}).order(post_id: 'desc').limit(POST_LIMIT_COUNT).offset(intPageOffsetNum)
-    getPagerCount(@aryPosts.count)
-    getLinkedTags
+    aryPosts = Post.joins(:taglinks).where(taglinks: {tag_id: params[:name]}).order(post_id: 'desc').limit(POST_LIMIT_COUNT).offset(intPageOffsetNum)
+    getPagerCount(aryPosts.count)
+    getShortPosts(aryPosts)
+    getRightColumnData
     slim :blog
   end
   get '/css/stylesheet.css' do
@@ -48,11 +48,42 @@ class MainApp < Sinatra::Base
   get '/js/javascript.js' do
     coffee :'js/javascript'
   end
-  def getLinkedTags()
-    # 右カラム、各記事に設定されたタグのデータを取得する.
+  def getShortPosts(aryPosts)
+    # 記事一覧表示用の投稿データを取得する.
+    @intCount = aryPosts.count - 1
+    @aryPostUrl = []
+    @aryPostTitle = []
+    @aryShortPost = []
+    @aryUpdateDate = []
 
-    # TODO: Taglinkは、現在表示している記事のみのタグを表示したい。
-    @aryTags = Tag.joins(:taglinks).select(:tag_name, :post_id, :tag_id)
+    @aryTagNames = []
+    @aryTagUrls = []
+
+    aryPosts.each do |post|
+      @aryPostUrl << POST_URL_DIR + post.post_id.to_s
+      @aryPostTitle << post.post_title
+      strShortPost = post.post
+      if strShortPost.size >= POST_SHORT_LIMIT_SIZE
+        strShortPost = strShortPost[0, POST_SHORT_LIMIT_SIZE - 1]
+        strShortPost += "…"
+      end
+      @aryShortPost.push(strShortPost)
+      @aryUpdateDate.push(post.updated_at)
+
+      aryTags = Tag.joins(:taglinks).select(:tag_name, :tag_id).where(taglinks: {post_id: post.post_id})
+      aryTagName = []
+      aryTagUrl = []
+      aryTags.each do |tag|
+        aryTagName << tag.tag_name
+        aryTagUrl << TAG_URL_DIR + tag.tag_id.to_s
+      end
+      @aryTagNames << aryTagName
+      @aryTagUrls << aryTagUrl
+    end
+  end
+  def getRightColumnData()
+    # 右カラムのデータを取得する.
+    # 追加済みのタグを取得する.
     @aryAllTags = Tag.all.order(tag_name: 'asc')
     # 最近の投稿から10件取得する.
     @aryCurrentPosts = Post.select(:post_id, :post_title).order(updated_at: 'desc').limit(10)
